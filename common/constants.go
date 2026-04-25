07cb0d03eedf41f533c894a34d9d966eb7b53b02
rebecca554owen <rebecca554owen@gmail.com>
2026-04-25 22:48:46 +0800

    feat: 支持独立日志库与旧日志异步迁移

--- common/constants.go
+++ common/constants.go
@@ -113,6 +113,9 @@ var DebugEnabled bool
 var MemoryCacheEnabled bool
 
 var LogConsumeEnabled = true
+var AutoMigrateOldLogsToLogDB bool
+var LogMigrationBatchSize int
+var AllowLogMigrationToNonEmptyTarget bool
 
 var TLSInsecureSkipVerify bool
 var InsecureTLSConfig = &tls.Config{InsecureSkipVerify: true}
--- main.go
+++ main.go
@@ -300,6 +300,7 @@ func InitResources() error {
 	if err != nil {
 		return err
 	}
+	model.MigrateOldLogsToLogDBIfNeeded()
 
 	// Initialize Redis
 	err = common.InitRedisClient()
