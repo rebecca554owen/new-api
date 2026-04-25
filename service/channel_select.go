9099a1e5519daafe447f4597d4f3392f3f851fa8
rebecca554owen <rebecca554owen@gmail.com>
2026-04-25 22:44:22 +0800

    feat: 优化原生渠道分组与失败重试选择

--- service/channel_select.go
+++ service/channel_select.go
@@ -12,11 +12,13 @@ import (
 )
 
 type RetryParam struct {
-	Ctx          *gin.Context
-	TokenGroup   string
-	ModelName    string
-	Retry        *int
-	resetNextTry bool
+	Ctx                   *gin.Context
+	TokenGroup            string
+	ModelName             string
+	Retry                 *int
+	resetNextTry          bool
+	PreferredChannelTypes []int // native channel types to prioritize based on request path
+	ExcludedChannelIds    []int // channels that have already failed in this request
 }
 
 func (p *RetryParam) GetRetry() int {
@@ -115,7 +117,7 @@ func CacheGetRandomSatisfiedChannel(param *RetryParam) (*model.Channel, string, 
 			}
 			logger.LogDebug(param.Ctx, "Auto selecting group: %s, priorityRetry: %d", autoGroup, priorityRetry)
 
-			channel, _ = model.GetRandomSatisfiedChannel(autoGroup, param.ModelName, priorityRetry)
+			channel, _ = model.GetRandomSatisfiedChannel(autoGroup, param.ModelName, priorityRetry, param.PreferredChannelTypes, param.ExcludedChannelIds)
 			if channel == nil {
 				// Current group has no available channel for this model, try next group
 				// 当前分组没有该模型的可用渠道，尝试下一个分组
@@ -153,7 +155,7 @@ func CacheGetRandomSatisfiedChannel(param *RetryParam) (*model.Channel, string, 
 			break
 		}
 	} else {
-		channel, err = model.GetRandomSatisfiedChannel(param.TokenGroup, param.ModelName, param.GetRetry())
+		channel, err = model.GetRandomSatisfiedChannel(param.TokenGroup, param.ModelName, param.GetRetry(), param.PreferredChannelTypes, param.ExcludedChannelIds)
 		if err != nil {
 			return nil, param.TokenGroup, err
 		}
