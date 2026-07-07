c3870c0aa619e2de202eef6d0a2f58a74d04f6a1
CaIon <i@caion.me>
2026-07-07 21:12:13 +0800

    refactor(price): improve handling of other ratios in PriceData
    (cherry picked from commit fc1259f583664668ff27809a6acfe1129e361272)

---
+++ relay/relay_task_test.go
@@ -0,0 +1,52 @@
+package relay
+
+import (
+	"math"
+	"testing"
+
+	relaycommon "github.com/QuantumNous/new-api/relay/common"
+	"github.com/QuantumNous/new-api/types"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRecalcQuotaFromRatiosIgnoresInvalidMultipliers(t *testing.T) {
+	info := &relaycommon.RelayInfo{
+		PriceData: types.PriceData{
+			Quota: 100,
+		},
+	}
+	info.PriceData.AddOtherRatio("duration", 2)
+
+	quota, ok := recalcQuotaFromRatios(info, map[string]float64{
+		"duration": 3,
+		"zero":     0,
+		"negative": -1,
+		"nan":      math.NaN(),
+		"inf":      math.Inf(1),
+	})
+
+	require.True(t, ok)
+	assert.Equal(t, 150, quota)
+	assert.True(t, info.PriceData.HasOtherRatio("duration"))
+}
+
+func TestRecalcQuotaFromRatiosRejectsAllInvalidAdjustedRatios(t *testing.T) {
+	info := &relaycommon.RelayInfo{
+		PriceData: types.PriceData{
+			Quota: 100,
+		},
+	}
+	info.PriceData.AddOtherRatio("duration", 2)
+
+	quota, ok := recalcQuotaFromRatios(info, map[string]float64{
+		"zero":     0,
+		"negative": -1,
+		"nan":      math.NaN(),
+		"inf":      math.Inf(1),
+	})
+
+	require.False(t, ok)
+	assert.Equal(t, 0, quota)
+	assert.True(t, info.PriceData.HasOtherRatio("duration"))
+}
